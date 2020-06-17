bb25ed8ad695f3718abbab76ae040d4d3c8d2e20
Saad Ibrahim <saad.lakhany@gmail.com>
2020-06-17 19:54:10 +0400

    Updates

---
+++ precache-manifest.91be5326245c8283e03b0cfd5f0b9d44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce710fcb193f20df5ecf5e4576f3195",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "2e72b3d6699e30fe1b6e",
+    "url": "/weather-app/static/js/2.f5e33be7.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/weather-app/static/js/2.f5e33be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3191644ef6fe2439a28",
+    "url": "/weather-app/static/js/main.937bfb8d.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  }
+]);
